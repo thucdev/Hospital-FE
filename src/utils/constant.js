87e068e4c87da9daca9b7259c9c542d85790a104
thucdo.developer@gmail.com <thucdo.developer@gmail.com>
2022-03-02 15:31:44 +0700

    change url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
-export const apiUrl =
-   process.env.NODE_ENV !== "production"
-      ? "http://localhost:8080"
-      : "https://hospital-backend-thucdev.herokuapp.com"
+export const apiUrl = process.env.REACT_APP_BACKEND_URL
+// process.env.NODE_ENV !== "production"
+//    ? "http://localhost:8080"
+//    : "https://hospital-backend-thucdev.herokuapp.com"
 
 export const ACCESS_TOKEN = "accessToken"
 export const REFRESH_TOKEN = "refreshToken"
